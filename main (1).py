a64866ae241ca1bef9a198239414eb4d7166190f
Vernisha8667 <144667406+Vernisha8667@users.noreply.github.com>
2023-09-20 11:36:38 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,21 @@
-n = int(input("Enter input number:"))
-fact = 1
-if n < 0:
-  print("factorial does not exist for neg")
-elif n == 0:
-  print("The factorial of 0 is 1")
-else:
-  for i in range(1, n + 1):
-    fact = fact * i
-  print("The factorial of", n, "is", fact)
+class player:
+  def play(self):
+    print("the player is playing cricket:")
+    
+#Define the drived class Bastman
+class Batsman(player):
+  def play(self):
+    print("the batsman is batting. ")
+    
+#Define the driver class Bowler
+class Bowler:
+  def play(self):
+    print("the bowler is Bowling")
+    
+#create object of Batsman and Bowler classes
+batsman=Batsman()
+bowler=Bowler() 
+
+#call the play() method for each object
+batsman.play() 
+bowler.play()
